d7e3ffa93de982b414a7eb41e5f9330acff49919
vlphzinho <140907152+vlphzinho@users.noreply.github.com>
2023-11-09 21:52:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stack_angular
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/vlphzinho/stack_angular)
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+   <header>
+     <h1>Hello from {{ name }}!</h1>
+    </header>
+    <section>
+      <header>
+        <h2>Poster</h2>
+      </header>
+       <article> 
+       <header>
+        <h3>Poste #1</h3>
+      </header>
+         <p>
+      
+         </p>
+       </article>
+    </section>
+    
+    
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
